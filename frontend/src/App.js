510556cdef2c8721a02d7f00b735f343521bd600
ka4an <n.kachanov@m-games-ltd.com>
2022-02-03 16:30:10 +0300

    #add #frontend - add tape

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,6 @@ function App() {
   return (
     <div>
       <IndexRouter />
-      <main className="main-container">
-        <header className="main-header">HEADER</header>
-        <div className="main-content">MAIN CONTENT</div>
-        <footer className="main-footer">FOOTER</footer>
-      </main>
     </div>
   );
 }
--- frontend/src/component/IndexPromoComponent/TapeComponent/TapeComponent.jsx
+++ frontend/src/component/IndexPromoComponent/TapeComponent/TapeComponent.jsx
@@ -6,10 +6,25 @@ function IndexTapeComponent() {
     <div className='tape-wrapper'>
       <div className='tape'>
         <div className='left-content'>
-
+          <div className='first-promo-row'>
+            <div className='left-column'>
+              <div className='main-menu'>
+                {/* main menu */}
+              </div>
+              <div className='reu-facts'>
+                {/* reu fatcts */}
+              </div>
+            </div>
+            <div className='right-column'>
+              {/* slider */}
+            </div>
+          </div>
+          <div className='tape-content'>
+            {/*  */}
+          </div>
         </div>
         <div className='right-bar'>
-          asdasfdafsdfs
+          {/*  */}
         </div>
       </div>
     </div>
