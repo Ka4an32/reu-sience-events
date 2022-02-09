954b9e60018c2cc5935d471ad9468fe40af702b3
ka4an <nickita.ka4anov@yandex.ru>
2022-02-10 00:46:32 +0300

    #add #frontend - add grid component

--- frontend/src/pages/IndexLayoutPage.jsx
+++ frontend/src/pages/IndexLayoutPage.jsx
@@ -1,91 +1,56 @@
+import { useState } from 'react';
 import Header from '../component/Header/Header';
 import './IndexLayoutPage.scss';
-
-import MenuList from '../constants/menu-list.json';
-import PhoneList from '../constants/phone-list.json';
-import EmailList from '../constants/eMail-list.json';
-import AdresList from '../constants/adress-list.json';
-
-import { NavLink } from 'react-router-dom';
-
-
-const renderPhoneList = () => {
-  return PhoneList.map(({ phone }, key) => {
-    const country = phone.slice(0, 2);
-    const region = phone.slice(2, 5);
-    const firstThreeNumber = phone.slice(5, 8);
-    const firstTwoNumber = phone.slice(8, 10);
-    const secondTwoNumber = phone.slice(10, 12);
-    return (
-      <a href={`tel:${phone}`} key={key}>
-        {`${country} (${region}) ${firstThreeNumber} - ${firstTwoNumber} - ${secondTwoNumber}`}
-      </a>
-    );
-  });
-};
-
-const renderMenuList = () => {
-  return MenuList.map((item, index) =>
-    <li key={index}>
-      <NavLink activeClassName={'active'} to={item.path}>{item.title}</NavLink>
-    </li>,
-  );
-};
-
-const renderEmailList = () => {
-  return EmailList.map(({ email }, index) => (
-    <a href={`mailto:${email}`} className={'mail'} key={index}>{email}</a>
-  ),
-  );
-};
-
-const renderAdresList = () => {
-  return AdresList.map(({ adres }, index) => (
-    <div className='korpus' key={index}>
-      {`Корпус ${index + 1}:`}
-      <div className='adres'>
-        {adres}
-      </div>
-    </div>
-  ),
-  );
-};
+import FullScreenNavbar from './Layouts/FullScreenNavbar/FullScreenNavbar';
 
 function IndexLayoutPage() {
+  window.innerWidth;
+  const [isFullScreen, setFullScreen] = useState(innerWidth < 1460 ? false : true);
+
+  window.onresize = () => {
+    setFullScreen(window.innerWidth < 1460 ? false : true);
+  };
+
   return (
-    <div className='main-promo-block'>
-      <div className='navbar-menu'>
-        <Header />
-        <nav className='block nav-menu'>
-          <h4 className='title'>МЕНЮ</h4>
-          <ul className='nav-link__list'>
-            {renderMenuList()}
-          </ul>
-        </nav>
-        <div className='block contact-block'>
-          <h4 className='title'>
-            Контакты
-          </h4>
-          <div className='border-block-wrapper'>
-            <div className='arrow-border-block'>
-              {renderPhoneList()}
-            </div>
-            <div className='hr' />
-            <div className='arrow-border-block'>
-              {renderEmailList()}
-            </div>
+    <div
+      className='main-promo-block'
+      style={
+        {
+          flexDirection: isFullScreen ? 'row' : 'column',
+          alignItems: isFullScreen ? 'flex-start' : 'center',
+        }
+      }
+    >
+      {
+        isFullScreen ? (
+          <FullScreenNavbar />
+        ) : (
+          <Header />
+        )
+      }
+      <div className='content-wrapper'>
+        <div className='static-block'>
+          <div className='block working-arrow'>
+            {/*  */}
+          </div>
+          <div className='block strategy'>
+            {/*  */}
+          </div>
+          <div className='block strategy'>
+            {/*  */}
+          </div>
+          <div className='block facts'>
+            {/*  */}
+          </div>
+          <div className='block slider'>
+            {/*  */}
+          </div>
+          <div className='block tape'>
+            {/*  */}
           </div>
         </div>
+        <div className='block sticky-block'>
 
-        <div className='block adres-block'>
-          <h4 className='title'>
-            Адрес
-          </h4>
-          <div className='border-block-wrapper'>
-            <div className='arrow-border-block'>
-              {renderAdresList()}
-            </div>
-          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Layouts/FullScreenNavbar/FullScreenNavbar.jsx
+++ frontend/src/pages/Layouts/FullScreenNavbar/FullScreenNavbar.jsx
@@ -1,3 +1,55 @@
+import Header from '../../../component/Header/Header';
+import './FullScreenNavbar.scss';
+
+import MenuList from '../../../constants/menu-list.json';
+import PhoneList from '../../../constants/phone-list.json';
+import EmailList from '../../../constants/eMail-list.json';
+import AdresList from '../../../constants/adress-list.json';
+
+import { NavLink } from 'react-router-dom';
+
+const renderPhoneList = () => {
+  return PhoneList.map(({ phone }, key) => {
+    const country = phone.slice(0, 2);
+    const region = phone.slice(2, 5);
+    const firstThreeNumber = phone.slice(5, 8);
+    const firstTwoNumber = phone.slice(8, 10);
+    const secondTwoNumber = phone.slice(10, 12);
+    return (
+      <a href={`tel:${phone}`} key={key}>
+        {`${country} (${region}) ${firstThreeNumber} - ${firstTwoNumber} - ${secondTwoNumber}`}
+      </a>
+    );
+  });
+};
+
+const renderMenuList = () => {
+  return MenuList.map((item, index) =>
+    <li key={index}>
+      <NavLink activeClassName={'active'} to={item.path}>{item.title}</NavLink>
+    </li>,
+  );
+};
+
+const renderEmailList = () => {
+  return EmailList.map(({ email }, index) => (
+    <a href={`mailto:${email}`} className={'mail'} key={index}>{email}</a>
+  ),
+  );
+};
+
+const renderAdresList = () => {
+  return AdresList.map(({ adres }, index) => (
+    <div className='korpus' key={index}>
+      {`Корпус ${index + 1}:`}
+      <div className='adres'>
+        {adres}
+      </div>
+    </div>
+  ),
+  );
+};
+
 function FullScreenNavbar() {
   return (
     <div className='navbar-menu'>
@@ -33,5 +85,7 @@ function FullScreenNavbar() {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default FullScreenNavbar;
